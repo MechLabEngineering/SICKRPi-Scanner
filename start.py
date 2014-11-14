7c50c29129d91eafec5fda8fb181e9ddaed0dfe0
gus484 <gustav484@web.de>
2014-11-14 08:59:48 +0100

    fix: wrong interrupt setting

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ import time
 
 HOST = "localhost"
 PORT = 4223
-UID = "7xwQ9g" # Change to your UID
+UID = "7xwQ9g"
 run = True
 is_scan = False
 
@@ -15,8 +15,11 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_industrial_digital_in_4 import IndustrialDigitalIn4
 
 #mount stick
-#subprocess.call(["sudo mount" , "/dev/sda1 /media/usb -o uid=pi,gid=pi"])
+#subprocess.call(["sudo mount" , "/dev/sdb1 /media/usbstick -o uid=pi,gid=pi"])
 
+################################################################################
+
+# thread: start laserscan
 def call_laser():
 	ret = subprocess.call("./main", shell=True)
 	
@@ -24,10 +27,8 @@ def call_laser():
 		global is_scan
 		is_scan = False
 
-# Callback function for interrupts
+# callback function for interrupts
 def cb_interrupt(interrupt_mask, value_mask):
-	#print('Interrupt by: ' + str(bin(interrupt_mask)))
-	#print('Value: ' + str(bin(value_mask)))
 	global is_scan
     
 	# check start interrupt
@@ -37,13 +38,20 @@ def cb_interrupt(interrupt_mask, value_mask):
 			is_scan = True
 			# start laser thred
 			start_new_thread(call_laser,())
+	# check stop interrupt
 	elif interrupt_mask & 0x02 and value_mask & 0x02:
 		if is_scan:
 			print "stop scan"
 			execfile("quit.py")
 			is_scan = False
+	# check shutdown interrupt
 	elif interrupt_mask & 0x03 and value_mask & 0x03:
+		run = False
 		subprocess.call(["shutdown" , "-h now"])
+
+################################################################################
+
+# init tinkerforge
 
 ipcon = IPConnection() # Create IP connection
 idi4 = IndustrialDigitalIn4(UID, ipcon) # Create device object
@@ -55,10 +63,9 @@ try:
 	# Register callback for interrupts
 	idi4.register_callback(idi4.CALLBACK_INTERRUPT, cb_interrupt)
 
-	# Enable interrupt on pin 0, 1, 2
-	idi4.set_interrupt(1 << 0)
-	idi4.set_interrupt(1 << 1)
-	idi4.set_interrupt(1 << 2)
+	# Enable interrupt on pin 0, 1
+	idi4.set_interrupt(0x03)	
+
 except:
 	print "Error!"
 	exit()
@@ -66,6 +73,7 @@ except:
 while run:
 	try:
 		time.sleep(0.05)
+		#print idi4.get_value()
 	except:
 		print "Quit"
 		run = False
