69e84e218f0f7f01ec67eded9c359edb7ecc67b6
Markus Fischer <gus484@gus484-ThinkPad-R61.(none)>
2015-01-21 08:47:37 +0100

    add check for file exist

--- tools/cpy_bin.sh
+++ tools/cpy_bin.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ $# -ne 2 ]
+  then
+    echo "not enough arguments supplied"
+    echo "cpy_bin.sh [PATH_TO_BIN_FILE] [PATH_TO_TARGET_DIRECTORY]"
+    exit 0
+fi
+
+if [ ! -e $1 ]; then
+    echo "file not found!"
+    exit 0
+fi
+
 echo "copy bin to usb"
 #copy file $1 to stick $2 
 sudo cp $1 $2
